bee1a8c56f3099de38136fe41a62321eb45bb96f
jquiceno <soyjquiceno@gmail.com>
2025-02-25 08:14:45 -0500

    app config added
    - new config directory created
    - changes in project configurations and environments

---
+++ src/config/configuration.ts
@@ -0,0 +1,22 @@
+import { registerAs } from '@nestjs/config';
+import { env } from 'node:process';
+
+export const configuration = registerAs('GLOBAL_CONFIG', () => {
+  const { FIRESTORE_PRIVATE_KEY, FIRESTORE_PRIVATE_KEY_BASE_64 } = env;
+
+  const fsPrivateKey = FIRESTORE_PRIVATE_KEY_BASE_64
+    ? Buffer.from(FIRESTORE_PRIVATE_KEY_BASE_64, 'base64').toString('ascii')
+    : FIRESTORE_PRIVATE_KEY;
+
+  return {
+    //App
+    NODE_ENV: process.env.NODE_ENV || 'dev',
+    PORT: parseInt(process.env.PORT || '3000'),
+
+
+    //Firestore
+    FIRESTORE_PROJECT_ID: process.env.FIRESTORE_PROJECT_ID,
+    FIRESTORE_PRIVATE_KEY: fsPrivateKey,
+    FIRESTORE_CLIENT_EMAIL: process.env.FIRESTORE_CLIENT_EMAIL,
+  };
+});
---
+++ src/config/validation-schema.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  // Application
+  NODE_ENV: Joi.string().valid('dev', 'production', 'test'),
+  PORT: Joi.number().default(6666),
+
+  //Firestore
+  FIRESTORE_PROJECT_ID: Joi.string().required(),
+  FIRESTORE_PRIVATE_KEY: Joi.string(),
+  FIRESTORE_CLIENT_EMAIL: Joi.string().required(),
+});
