e37d8ab1779c0a54bdde3ea6904005b10166b10a
jquiceno <soyjquiceno@gmail.com>
2025-03-05 14:45:30 -0500

    validation pipe created, swagger ui added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { configuration, GlobalConfigType } from './config';
 import { AppModule } from './app.module';
@@ -12,6 +14,26 @@ async function bootstrap() {
   logger = await app.resolve<Logger>(Logger);
 
   const config = await app.resolve<GlobalConfigType>(configuration.KEY);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remueve propiedades no decoradas
+      transform: true, // Transforma los datos al tipo especificado
+      forbidNonWhitelisted: true, // Lanza error si hay propiedades no permitidas
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Spider Football API')
+    .setDescription(
+      'A powerful REST API for retrieving football (soccer) data including leagues, matches, and teams.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
 
   await app.listen(config.PORT);
 
