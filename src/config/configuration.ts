180f8d541be885c14dfd02a6ad1b5de50a274683
jquiceno <soyjquiceno@gmail.com>
2025-07-20 11:54:03 +0200

    Unnecessary config removed

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,25 +1,10 @@
 import { registerAs } from '@nestjs/config';
-import { env } from 'node:process';
 
 export const configuration = registerAs('GLOBAL_CONFIG', () => {
-  const { FIRESTORE_PRIVATE_KEY, FIRESTORE_PRIVATE_KEY_BASE_64 } = env;
-
-  const fsPrivateKey = FIRESTORE_PRIVATE_KEY_BASE_64
-    ? Buffer.from(FIRESTORE_PRIVATE_KEY_BASE_64, 'base64').toString('ascii')
-    : FIRESTORE_PRIVATE_KEY;
-
   return {
     //App
     NODE_ENV: process.env.NODE_ENV || 'dev',
     PORT: parseInt(process.env.PORT || '8080', 10),
-
-    //Firestore
-    FIRESTORE_PROJECT_ID: process.env.FIRESTORE_PROJECT_ID,
-    FIRESTORE_PRIVATE_KEY: fsPrivateKey,
-    FIRESTORE_CLIENT_EMAIL: process.env.FIRESTORE_CLIENT_EMAIL,
-
-    //Auth
-    JWT_SECRET: process.env.JWT_SECRET || 'private_secret',
 
     //Mongo
     MONGO_URI: process.env.MONGO_URI,
--- src/config/validation-schema.ts
+++ src/config/validation-schema.ts
@@ -5,14 +5,6 @@ export const validationSchema = Joi.object({
   NODE_ENV: Joi.string().valid('dev', 'production', 'test'),
   PORT: Joi.number().default(8080),
 
-  //Firestore
-  FIRESTORE_PROJECT_ID: Joi.string().required(),
-  FIRESTORE_PRIVATE_KEY: Joi.string(),
-  FIRESTORE_CLIENT_EMAIL: Joi.string().required(),
-
-  //App
-  JWT_SECRET: Joi.string().required(),
-
   //Mongo
   MONGO_URI: Joi.string().required(),
 });
