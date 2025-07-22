3e9f5d4d7cce366fc598327fc0468101ed166160
jquiceno <soyjquiceno@gmail.com>
2025-07-22 22:51:50 +0200

    docs server url added to config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,12 +1,25 @@
 import { registerAs } from '@nestjs/config';
 
 export const configuration = registerAs('GLOBAL_CONFIG', () => {
+  const NODE_ENV = process.env.NODE_ENV;
+
+  const DOCS_SERVER_URL =
+    NODE_ENV === 'dev'
+      ? `http://localhost:${process.env.PORT}`
+      : process.env.DOCS_SERVER_URL || '';
+
   return {
-    //App
-    NODE_ENV: process.env.NODE_ENV || 'dev',
+    // App
+    NODE_ENV,
     PORT: parseInt(process.env.PORT || '8080', 10),
 
-    //Mongo
+    // Mongo
     MONGO_URI: process.env.MONGO_URI,
+
+    // Logging
+    LOG_LEVEL: process.env.LOG_LEVEL || 'info',
+
+    // Docs
+    DOCS_SERVER_URL,
   };
 });
